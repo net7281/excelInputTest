333222a9eebe98665a89098005162ed8954959ec
정현정 <33312138+net7281@users.noreply.github.com>
2023-02-20 09:50:05 +0900

    엑셀 양식 구현

--- src/main/java/com/excelText/excelInputTest/MainController.java
+++ src/main/java/com/excelText/excelInputTest/MainController.java
@@ -1,7 +1,12 @@
 package com.excelText.excelInputTest;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -10,16 +15,20 @@ import java.util.Map;
 
 import javax.inject.Inject;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class MainController {
@@ -37,7 +46,7 @@ public class MainController {
 	@RequestMapping(value="/upload", method = RequestMethod.POST)
 	public String upload(RedirectAttributes redirectAttributes, MultipartHttpServletRequest multiRequest, ModelMap model) {
 		
-		//걸린시간용
+		//걸린시간시작
  		long beforeTime = System.currentTimeMillis();
 	
 		MultipartFile excelFile = multiRequest.getFile("excelFile");
@@ -79,11 +88,32 @@ public class MainController {
 	}
 	
 	@RequestMapping(value="/download")
-	public String download() {
-		String fileName = "엑셀업로드테스트_양식.xlsx";
-		String saveFileName = "c:/tmp/엑셀업로드테스트_양식.xlsx";
-		String contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; charset=UTF-8";
-		return contentType;
-		
+	public String download(@RequestParam("name") String name, HttpServletResponse response) throws Exception {
+		String fileName = new String();
+		if(name.equals("엑셀업로드테스트_양식.xlsx")) {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+			Calendar c1 = Calendar.getInstance();
+			String strToday = sdf.format(c1.getTime()) ;
+			fileName = new String((strToday+"_"+name).getBytes("UTF-8"),"ISO-8859-1");
+		}else {
+			fileName = new String(name.getBytes("UTF-8"),"ISO-8859-1");
+		}
+		try {
+			File file = ResourceUtils.getFile("classpath:static/file/"+name);
+			response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; charset=UTF-8");
+			response.setHeader("Content-Disposition", "attachment;filename="+fileName);
+			response.setHeader("Content-Transfer-Encoding", "binary");
+			
+			byte[] fileByte = FileUtils.readFileToByteArray(file);
+			
+			response.getOutputStream().write(fileByte);
+			response.getOutputStream().flush();
+			response.getOutputStream().close();
+			
+			System.out.println(file.exists());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
